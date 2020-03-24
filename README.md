d6534f9e07fafbb490848b047540ec8e8c6d02e0
Cynaith <894324762@qq.com>
2020-03-25 02:25:09 +0800

    集成BaiduMap 实现检索POI功能

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # iMart
 ---
 - 帖子数据 https://www.rouding.com/shejiyushoucang/
+- 帖子数据 https://www.topys.cn/
 - 首页数据 http://search.sohu.com/?keyword=%E5%88%9B%E6%84%8F%E5%B8%82%E9%9B%86&spm=smpc.csrpage.0.0.1581866449821X7uHqPs&queryType=edit
 - 图标 https://www.iconfont.cn/collections/detail?spm=a313x.7781069.1998910419.d9df05512&cid=20933
  
@@ -10,3 +11,22 @@
 - 全局变量 SharePreferenceUtils.getInstance().getUserName()
 - 点赞https://github.com/jd-alexander/LikeButton
 - 收藏 https://github.com/varunest/SparkButton
+
+---
+
+- 推荐功能
+
+  - 后台根据定位获取相关 Shop
+    - 前台获取定位，包含Location 和City
+    - 后台根据Location 获取 region(完成)
+    - 后台根据Location 获取 Shop (完成)
+  - 后台根据 当前 Location和目标 Location 实现 WalkingWay (完成)
+    - 返回List<String>
+  - 后台根据 当前 Location和目标 Location 实现 BusWay
+  - 后台根据 当前 Location和目标 Location 实现 CarWay
+  - 集成百度地图实现POI检索 (完成)
+  - 界面
+    - RecycleView 展示 List
+    - List -> TabLayout Walk、Bus、Car
+    - List展示、百度Map
+
---
+++ app/src/main/java/com/ly/imart/view/Fourth/DiyActivity.java
@@ -0,0 +1,124 @@
+package com.ly.imart.view.Fourth;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.baidu.mapapi.CoordType;
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.model.LatLng;
+import com.baidu.mapapi.search.core.PoiInfo;
+import com.baidu.mapapi.search.core.SearchResult;
+import com.baidu.mapapi.search.poi.OnGetPoiSearchResultListener;
+import com.baidu.mapapi.search.poi.PoiCitySearchOption;
+import com.baidu.mapapi.search.poi.PoiDetailResult;
+import com.baidu.mapapi.search.poi.PoiDetailSearchOption;
+import com.baidu.mapapi.search.poi.PoiDetailSearchResult;
+import com.baidu.mapapi.search.poi.PoiIndoorResult;
+import com.baidu.mapapi.search.poi.PoiNearbySearchOption;
+import com.baidu.mapapi.search.poi.PoiResult;
+import com.baidu.mapapi.search.poi.PoiSearch;
+import com.ly.imart.R;
+import com.ly.imart.overlayutil.PoiOverlay;
+import com.ly.imart.util.SystemInfo;
+
+import java.util.Arrays;
+
+public class DiyActivity extends Activity implements View.OnClickListener {
+
+    private MapView mMapView;
+    private PoiSearch mPoiSearch = null;
+    private BaiduMap mBaiduMap = null;
+    Button button;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_diy);
+        mMapView = (MapView) findViewById(R.id.bmapView);
+        mPoiSearch = PoiSearch.newInstance();
+        button = findViewById(R.id.test);
+        button.setOnClickListener(this);
+        mBaiduMap = mMapView.getMap();
+        OnGetPoiSearchResultListener listener = new OnGetPoiSearchResultListener() {
+            @Override
+            public void onGetPoiResult(PoiResult poiResult) {
+                if (poiResult.error == SearchResult.ERRORNO.NO_ERROR) {
+                    mBaiduMap.clear();
+
+                    //创建PoiOverlay对象
+                    PoiOverlay poiOverlay = new PoiOverlay(mBaiduMap);
+
+                    //设置Poi检索数据
+                    poiOverlay.setData(poiResult);
+
+                    //将poiOverlay添加至地图并缩放至合适级别
+                    poiOverlay.addToMap();
+                    poiOverlay.zoomToSpan();
+                }
+            }
+
+            @Override
+            public void onGetPoiDetailResult(PoiDetailResult poiDetailResult) {
+
+            }
+
+            @Override
+            public void onGetPoiDetailResult(PoiDetailSearchResult poiDetailSearchResult) {
+
+            }
+            @Override
+            public void onGetPoiIndoorResult(PoiIndoorResult poiIndoorResult) {
+
+            }
+
+        };
+        mPoiSearch.setOnGetPoiSearchResultListener(listener);
+
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
+        mMapView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
+        mMapView.onPause();
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId()==R.id.test){
+            mPoiSearch.searchInCity((new PoiCitySearchOption())
+                    .city("北京")
+                    .keyword("ktv")
+                    .pageNum(10)
+                    .scope(1));
+        }
+    }
+}
+
