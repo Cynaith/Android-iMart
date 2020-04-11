3d9d706c3a4daa7b82b9533d7f0bbd995b303a75
Cynaith <894324762@qq.com>
2020-04-12 00:52:05 +0800

    Update MyshowModelImpl.java

--- app/src/main/java/com/ly/imart/model/Fourth/MyshowModelImpl.java
+++ app/src/main/java/com/ly/imart/model/Fourth/MyshowModelImpl.java
@@ -122,7 +122,7 @@ public class MyshowModelImpl implements IMyshowModel {
         public String call() throws Exception {
             String accessToken = "";
             OkHttpRequest okHttpRequest = new OkHttpRequest();
-            String accessTokenUrl = "http://10.0.2.2:8080/user/followUser?followName="+userName+"&userName="+ SharePreferenceUtils.getInstance().getUserName();
+            String accessTokenUrl = "http://47.101.171.252:8080/user/followUser?followName="+userName+"&userName="+ SharePreferenceUtils.getInstance().getUserName();
             try {
                 //发送请求
                 accessToken = okHttpRequest.get(accessTokenUrl);
